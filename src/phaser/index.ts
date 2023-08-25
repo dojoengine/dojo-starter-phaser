f20cc5fdd33c8724817d880030b52a9763a8a000
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-08-25 11:03:29 +1000

    fix sprite

--- src/phaser/index.ts
+++ src/phaser/index.ts
@@ -6,16 +6,16 @@ import { namespaceWorld } from '@latticexyz/recs';
 export type PhaserLayer = Awaited<ReturnType<typeof createPhaserLayer>>;
 type PhaserEngineConfig = Parameters<typeof createPhaserEngine>[0];
 
-// type Props = {
-//     networkLayer: NetworkLayer;
-// };
-
 export const createPhaserLayer = async (networkLayer: NetworkLayer, phaserConfig: PhaserEngineConfig) => {
 
     const world = namespaceWorld(networkLayer.world, "phaser");
     const { game, scenes, dispose: disposePhaser } = await createPhaserEngine(phaserConfig);
     world.registerDisposer(disposePhaser);
 
+    const { camera } = scenes.Main;
+
+    camera.phaserCamera.setBounds(-1000, -1000, 2000, 2000);
+    camera.phaserCamera.centerOn(0, 0);
 
     const components = {};
 
--- src/phaser/systems/move.ts
+++ src/phaser/systems/move.ts
@@ -4,6 +4,7 @@ import { Animations, Sprites } from "../constants";
 export const move = (layer: PhaserLayer) => {
 
     const {
+        game,
         world,
         scenes: {
             Main: { phaserScene, config },
@@ -13,7 +14,7 @@ export const move = (layer: PhaserLayer) => {
             account
         },
     } = layer;
-    const soliderSprite = config.sprites[Sprites.Soldier];
+
     const soldier = phaserScene.add
         .sprite(0, 0, "soldier")
         .play(Animations.SwordsmanIdle)
@@ -25,35 +26,8 @@ export const move = (layer: PhaserLayer) => {
 
     let tween: Phaser.Tweens.Tween | undefined;
 
-    const particles = phaserScene.add.particles(soliderSprite.assetKey);
-
-    // const emitter = particles.createEmitter({
-    //     x: soldier.x,
-    //     y: soldier.y,
-    //     speed: { min: -400, max: 400 },
-    //     angle: { min: 0, max: 360 },
-    //     scale: { start: 0.5, end: 0 },
-    //     blendMode: "SCREEN",
-    //     lifespan: 600,
-    //     gravityY: 800,
-    //     quantity: 20,
-    // });
-
-    soldier.on("pointerdown", async () => {
-        if (tween) return;
-
+    phaserScene.input.on("pointerdown", () => {
         spawn(account);
-
-        tween = phaserScene.add.tween({
-            targets: soldier,
-            duration: 250,
-            alpha: 0.5,
-            yoyo: true,
-            repeat: -1,
-            ease: "Sine.easeInOut",
-        });
+        soldier.setPosition(phaserScene.input.mousePointer?.x, phaserScene.input.mousePointer?.y);
     });
-
-
-
 };
